23c67606e82aa99781813e0ab31b45df2dd75a6e
optikfluffel <optik@fluffel.io>
2017-12-16 17:16:16 +0100

    straighten out tables in generated README
    because the escaping `\` shift everything a little bit in the output

--- index.js
+++ index.js
@@ -64,17 +64,17 @@ exports.writeReadme = function (dir, cb) {
     ## Routes
     Route              | File               | Description                     |
     -------------------|--------------------|---------------------------------|
-    \`/\`              | \`views/main.js\`  | The main view
-    \`/*\`             | \`views/404.js\`   | Display unhandled routes
+    \`/\`                | \`views/main.js\`    | The main view
+    \`/*\`               | \`views/404.js\`     | Display unhandled routes
 
     ## Commands
     Command                | Description                                      |
     -----------------------|--------------------------------------------------|
-    \`$ npm start\`        | Start the development server
-    \`$ npm test\`         | Lint, validate deps & run tests
-    \`$ npm run build\`    | Compile all files into \`dist/\`
-    \`$ npm run create\`   | Generate a scaffold file
-    \`$ npm run inspect\`  | Inspect the bundle's dependencies
+    \`$ npm start\`          | Start the development server
+    \`$ npm test\`           | Lint, validate deps & run tests
+    \`$ npm run build\`      | Compile all files into \`dist/\`
+    \`$ npm run create\`     | Generate a scaffold file
+    \`$ npm run inspect\`    | Inspect the bundle's dependencies
   `
 
   write(filename, file, cb)
