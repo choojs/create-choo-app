26dd05d27cd86e7bbb901e13faf59207df5d1ae8
Udo Kramer <optik@fluffel.io>
2017-12-18 20:38:11 +0100

    Cosmetic tweaks (#42)
    * straighten out tables in generated README
    
    because the escaping `\` shift everything a little bit in the output
    
    * add a div around the back link
    
    so it’s nicely aligned with the headline

--- index.js
+++ index.js
@@ -64,17 +64,17 @@ exports.writeReadme = function (dir, cb) {
     ## Routes
     Route              | File               | Description                     |
     -------------------|--------------------|---------------------------------|
-    \`/\`              | \`views/main.js\`  | The main view
-    \`/*\`             | \`views/404.js\`   | Display unhandled routes
+    \`/\`                | \`views/main.js\`    | The main view
+    \`/*\`               | \`views/404.js\`     | Display unhandled routes
 
     ## Commands
     Command                | Description                                      |
     -----------------------|--------------------------------------------------|
-    \`$ npm start\`        | Start the development server
-    \`$ npm test\`         | Lint, validate deps & run tests
-    \`$ npm run build\`    | Compile all files into \`dist/\`
-    \`$ npm run create\`   | Generate a scaffold file
-    \`$ npm run inspect\`  | Inspect the bundle's dependencies
+    \`$ npm start\`          | Start the development server
+    \`$ npm test\`           | Lint, validate deps & run tests
+    \`$ npm run build\`      | Compile all files into \`dist/\`
+    \`$ npm run create\`     | Generate a scaffold file
+    \`$ npm run inspect\`    | Inspect the bundle's dependencies
   `
 
   write(filename, file, cb)
@@ -191,9 +191,11 @@ exports.writeNotFoundView = function (dir, cb) {
           <h1 class="f-headline pa3 pa4-ns">
             404 - route not found
           </h1>
-          <a href="/" class="link black underline">
-            Back to main
-          </a>
+          <div class="ph3 ph4-ns">
+            <a href="/" class="link black underline">
+              Back to main
+            </a>
+          </div>
         </body>
       \`
     }\n
